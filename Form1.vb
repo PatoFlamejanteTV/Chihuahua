ddbc02f060d224a8c21660fee096ea8a1b50bfa5
PatoFlamejanteTV <flamejantepato77@gmail.com>
2024-12-14 12:34:17 -0300

    V1.0

---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    'SFXs / Music by Toby Fox, for the game UNDERTALE
+    'Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+    '    My.Computer.Audio.Play(My.Resources.greaterdog, AudioPlayMode.Background)
+    'End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'loop when the form loads (program starts)
+        My.Computer.Audio.Play(My.Resources.dogsong, AudioPlayMode.BackgroundLoop)
+    End Sub
+End Class
